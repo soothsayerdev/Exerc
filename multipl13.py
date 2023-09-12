f3844e7b03c3e70cba8b5952c40851ab54a39d25
Diogo L Andrade <127452332+digas019@users.noreply.github.com>
2023-09-11 22:42:00 -0300

    beecrowd
    exercicio de Multiplos de 13 soma

---
+++ multipl13.py
@@ -0,0 +1,14 @@
+num1 = int(input("Digite seu primeiro numero: "))
+num2 = int(input("Digite seu segundo numero: "))
+j = 0
+
+for i in range(num1,num2 + 1):
+    if (i % 13 != 0):
+        j += i
+        
+        
+print(j)
+    
+    
+
+    
