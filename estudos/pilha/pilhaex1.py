0e52ca0c39105689c02c14b7e0fbebf0aba14b9f
digas019 <127452332+digas019@users.noreply.github.com>
2023-10-01 16:58:22 -0300

    finalizando o exemplo pilha 01/10

--- estudos/pilha/pilhaex1.py
+++ estudos/pilha/pilhaex1.py
@@ -9,12 +9,43 @@ class Stack:
     def push(self, elem):
         # insere um elemento na pilha
         pass
-    
+        node = Node(elem)
+        node.next = self.top
+        self.top = node
+        self._size = self._size + 1
+
+
     def pop(self):
         # remove o elemento do topo da pilha 
+        if self.size > 0:
+            node = self.top
+            self.top = self.top.next
+            self._size = self._size - 1
+            return node.data
+        raise IndexError("The stack is empyty")
     
     def peek(self):
         # retorna o topo da pilha sem remover
+        if self.size > 0:
+            return self.top.data
+        raise IndexError("The stack is empty")
+
+
+ 
+    def __len__(self):
+        """Retorna o tamanho da lista"""
+        return self._size
+    
+
+    def __repr__(self):
+        r = ""
+        pointer = self.top
+        while(pointer):
+            r = r + str(pointer.data) + "\n"
+            pointer = pointer.next
+        return r
+    
+
 
 #    def append(self,elem):
 #        if self.head:
@@ -27,7 +58,3 @@ class Stack:
 #            # primeira insercao
 #            self.head = Node(elem)
 #        self._size = self._size + 1
- 
-    def __len__(self):
-        """Retorna o tamanho da lista"""
-        return self._size
