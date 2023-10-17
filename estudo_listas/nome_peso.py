8f84b9cfb8a61c9afe77c9440e214e55400bc200
digas019 <127452332+soothsayerdev@users.noreply.github.com>
2023-10-16 21:04:19 -0300

    create nome_peso exerc listas
    16/10 estudo sobre listas

---
+++ estudo_listas/nome_peso.py
@@ -0,0 +1,33 @@
+temp = []
+princ = []
+
+while True:
+    temp.append(str(input('Nome: ')))
+    temp.append(float(input('Peso: ')))
+    if len(princ) == 0:
+        mai = men = temp[1]
+    else:
+        if temp[1] > mai:
+            mai = temp[1]
+        if temp[1] < men:
+            men = temp[1]
+
+    princ.append(temp[:])
+    temp.clear()
+
+    resp = str(input('Quer Continuar? [S/N] '))
+    if resp in 'Nn':
+        break
+print('-=' * 30)
+print(f"Os dados foram {princ}")
+print(f"Ao todo, voce cadastrou {len(princ)} pessoas. ", end="")
+print(f"O maior peso foi de {mai} Kg. Peso de ", end="")
+for p in princ:
+    if p[1] == mai:
+        print(f"[{p[0]}]", end="")
+        
+print()
+print(f"O menor peso foi de {men} Kg")
+for p in princ:
+    if p[1] == men:
+        print(f"[{p[0]}] ", end="")
